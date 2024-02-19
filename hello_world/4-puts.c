0201eb431084084e1e11c2e1d99b8d7b86c1e0de
Your Name <saul.vera787@gmail.com>
2024-02-19 17:46:59 -0400

    Hello,puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,12 +2,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - this is the main put function
+ * Return: 0
  */
 int main(void)
 {
-   puts("\"Programming is like building a multilingual puzzle");
-                 return (0)
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
