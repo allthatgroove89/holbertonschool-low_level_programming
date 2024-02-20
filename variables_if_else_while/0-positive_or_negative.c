c61fc8cbc722fcf5f44c25231b26f4ff86ba2c33
Your Name <saul.vera787@gmail.com>
2024-02-20 15:07:08 -0400

    Complile update

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -16,9 +16,9 @@ int main(void)
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
-		printf("%d is negative\n", n);
-	else
 		printf("%d is zero\n", n);
-	return (0);
+	else
+		printf("%d is negative\n", n);
 
+	return (0);
 }
