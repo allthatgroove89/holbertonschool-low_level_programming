99b64b942c0038342f3186e1651ffe3f11f8c5b8
Your Name <saul.vera787@gmail.com>
2024-02-21 18:44:39 -0400

    Error fixed

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - check the code
  *
- * Return: Always 0.
+ * Return: void.
  */
 
 void print_alphabet(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 	_putchar(c);
