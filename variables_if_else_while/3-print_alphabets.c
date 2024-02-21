0b195b7b9f5eed3a3d90eaf8de9a67d2f5ed502a
Your Name <saul.vera787@gmail.com>
2024-02-20 21:58:19 -0400

    Task 3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
+
 /**
- *main-prints alphabet excluding q and e
+ *  main - program that prints the alphabet in lowercase and upper case
  *
- *Return: always zero (success)
+ *  Return: Always 0.
  */
 
 int main(void)
 {
-	char ch;
+	char letter;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-		if (ch != 'q' && ch != 'e')
-			putchar(ch);
-	}
+	for (letter = 'a'; letter <= 'z'; letter++)
+	putchar(letter);
 
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	putchar(letter);
 	putchar('\n');
-
 	return (0);
 }
+
