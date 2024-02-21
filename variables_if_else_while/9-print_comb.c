ae971295202cd9adf5e556aa16a1a8829e02f85c
Your Name <saul.vera787@gmail.com>
2024-02-20 22:02:57 -0400

    Task 9 Update

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 
+
 /**
- * main - prints all single digit nu,ber from 0
+ * main- prints all possible combinations of single-digit numbers
  *
- * Return: Always 0
+ * Return: Always 0.
+ *
  */
 
-int main(void)
+	int main(void)
 {
-	int i;
+		int num;
 
-	for (i = 0; i < 10; i++)
+	for (num = 0; num <= 9; num++)
 	{
-		putchar(i + '0');
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+
 	}
+
 	putchar('\n');
 	return (0);
 }
+
