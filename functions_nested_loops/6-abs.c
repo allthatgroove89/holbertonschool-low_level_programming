b30adcb24acdefd0656966e27a2f5a7d6dc3a979
Your Name <saul.vera787@gmail.com>
2024-02-21 20:11:17 -0400

    Task 6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -6,13 +6,16 @@
  * @c: The number to be computed
  * Return: Absolute value
  */
+
 int _abs(int c)
 {
 	if (c < 0)
-		int abs_val;
+	{
+	int abs_val;
 
 	abs_val = c * -1;
-		return (abs_val);
+	return (abs_val);
+	}
 	return (c);
 }
 
