ce5cb6ba5ec7a0712b6a3fb045a031f82151c1f1
Your Name <saul.vera787@gmail.com>
2024-02-22 23:54:13 -0400

    more numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 10 times the numbers from 0 to 14
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int t;
+	int i;
+
+	for (t = 0; t <= 9; t++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+				_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
