b04af4951895731f7099c57de4b088833f5eb0b6
Your Name <saul.vera787@gmail.com>
2024-02-26 18:53:04 -0400

    Length of a string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen(char *s) - Returns the length of a string
+ * @s: - Integet to be swapped.
+ * Return - is 0
+ *
+ */
+
+int_strlen(char *s)
+{
+
+	int lenght = 0;
+
+	while (*s != '0\')
+	{
+	length++;
+	s++;
+	}
+	return lenght;
+}
