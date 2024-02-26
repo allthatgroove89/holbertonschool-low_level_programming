4593da1286e32f54efc80b8f7aa374ee1e84b78f
Your Name <saul.vera787@gmail.com>
2024-02-26 14:45:15 -0400

    Pointers and arrays

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Takes a pointer to an int and update the value to 98.
+ * @n: - An integer input pointer
+ * Return - Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
