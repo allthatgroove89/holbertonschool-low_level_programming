e68f29d177babed6e7ba235fbf3723e69958db03
Your Name <saul.vera787@gmail.com>
2024-03-04 20:05:50 -0400

    factorial

---
+++ recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * factorial -return the factorial of a given number
+ * @n: tracks the factorial number
+ * Return: 0 or -1 to indicate an error
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
