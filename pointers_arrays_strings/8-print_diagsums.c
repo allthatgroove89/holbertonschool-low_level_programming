5566dceddc31501beaf78f6d9790bf92dfaa1685
Your Name <saul.vera787@gmail.com>
2024-03-06 09:23:40 -0400

    making some changes

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -8,17 +8,14 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j;
+	int i;
 	int sum1 = 0;
 	int sum2 = 0;
 
-	for (i = 0; i <= size * size; i += size + 1)
+	for (i = 0; i < size; i++)
 	{
-		sum1 += a[i];
-	}
-	for (j = 0; j < size * size - 1; j += size - 1)
-	{
-		sum2 += a[j];
+		sum1 += a[i * (size + 1)];
+		sum2 += a[(i + 1) * (size - 1)];
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
