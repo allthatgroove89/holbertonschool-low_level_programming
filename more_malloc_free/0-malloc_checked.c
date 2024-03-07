8caab83b290bc3f2923404f4ba69dfeea93f36e6
Your Name <saul.vera787@gmail.com>
2024-03-07 16:21:50 -0400

    C-more malloc proyect

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "main.h"
+
+ /**
+ * malloc_checked - allocates memory using malloc
+ * @b: counts elements
+ * Return: if malloc fails function should cause normal processs of termination
+ */
+
+void *malloc_checked(unsigned int b);
+{
+	void *mem;
+
+	if (b == 0)
+		exit(98);
+
+	mem = malloc(b);
+
+	if (mem == NULL)
+		exit(98);
+
+	return (mem);
+}
