714fdcfbfa2336cef4cfe338a72fe3000361579d
Your Name <saul.vera787@gmail.com>
2024-03-07 19:47:27 -0400

    concat string task

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -10,34 +10,34 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, len1, len2, sum = 0;
-	char *ptr;
+char *ptr;
+	unsigned int len1 = 0, len2 = 0, i = 0, j = 0;
 
-	if (s1 == 0)
-		s1 = "";
-	if (s2 == 0)
-		s2 = "";
-	for (len1 = 0; s1[len1] != '\0'; len1++)
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
-		for (len2 = 0; s2[len2] != '\0'; len2++)
+	if (n < len2)
+		len2 = n;
 
-			if (n >= len2)
-				sum = len1 + len2;
-			else
-				sum = len1 + n;
+	ptr = malloc(len1 + len2 + 1);
+	if (ptr == NULL)
+		return (NULL);
 
-	ptr = malloc(sizeof(char) * sum + 1);
-	if (ptr == 0)
-		return (0);
-	for (i = 0; i < len1; i++)
+	while (i < len1)
 	{
 		ptr[i] = s1[i];
-	}
-	for (len2 = 0; len2 < n && s2[len2] != '\0'; len2++)
-	{
-		ptr[i] = s2[len2];
 		i++;
 	}
+
+	while (i < (len1 + len2))
+	{
+		ptr[i] = s2[j];
+		i++;
+		j++;
+	}
 	ptr[i] = '\0';
+
 	return (ptr);
 }
