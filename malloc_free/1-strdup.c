b2d2e0ff8047c8ffa7ae468774f18cb3b182b33a
Your Name <saul.vera787@gmail.com>
2024-03-06 21:47:45 -0400

    1. The woman who has no imagination has no wings

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+/**
+ * _strdup - returns a pointer to a new string which is a duplicate of astring
+ * memory of the new string is obtained with malloc and can be freed with free
+ * @str: string to be duplicated
+ * Return: NULL if str = NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *dup_str;
+	char *ptr;
+	int size;
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+
+	size = _strlen(str);
+	dup_str = malloc(sizeof(char) * size + 1);
+	ptr = dup_str;
+
+	if (ptr == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		while (*str)
+		{
+			*ptr = *str;
+			ptr++;
+			str++;
+		}
+	}
+	return (dup_str);
+	free(dup_str);
+}
+
+/**
+ * _strlen - gets the lenght of a string
+ * @s: string to be measure
+ * Return: Lenght
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
