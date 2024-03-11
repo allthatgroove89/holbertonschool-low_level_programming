b99ef670a21bccbf05f5dd5695b8ce69e435e377
Your Name <saul.vera787@gmail.com>
2024-03-11 16:12:01 -0400

    poppy task

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,3 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+typedef struct dog my_dog;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
