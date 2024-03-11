b2bfa7a2a4a0a8162debe401881d5db3bc71e520
Your Name <saul.vera787@gmail.com>
2024-03-11 18:16:57 -0400

    correcting syntax mistakes

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,22 +1,33 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
 #include <stdio.h>
-#include "dog.h"
+#include <stdlib.h>
 
 /**
- * main - check the code
+ * struct dog - struct with floowing element
+ * @name: name of the dog
+ * @owner: name of the dog owner
+ * @age: age of the dog
  *
- * Return: Always 0.
+ * Description: long description
  */
+
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+char *name;
+char *owner;
+float age;
 };
-typedef struct dog my_dog;
+typedef struct dog dog_t;
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
+
+char *_strcpy(char *s);
+int _strlen(char *s);
 
