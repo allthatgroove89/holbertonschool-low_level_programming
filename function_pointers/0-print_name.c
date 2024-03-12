1fd945bc3d6ffeb58868403f098bcf05a529bcff
Your Name <saul.vera787@gmail.com>
2024-03-12 16:48:02 -0400

    C-Function Pointers Project

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+
+/**
+ * print_name - a function that prints a name
+ * @name: name of person
+ * @f: pointer to function
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
