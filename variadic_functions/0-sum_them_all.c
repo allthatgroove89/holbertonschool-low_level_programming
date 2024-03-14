7fadb2b21f1a3a372a617fb5df2f9580f85848b2
Your Name <saul.vera787@gmail.com>
2024-03-14 18:52:33 -0400

    editing betty style

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -8,7 +8,7 @@
  * Return: 0 if n is equal 0
  */
 
-int sum_them_all(const unsigned int n,...)
+int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
 	int total = 0;
@@ -16,6 +16,7 @@ int sum_them_all(const unsigned int n,...)
 
 	va_list args;
 	va_start(args, n);
+
 
 	if (n == 0)
 		return(0);
