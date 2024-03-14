715e57008e1ab638ede9b02b78bb20e1bd7d379d
Your Name <saul.vera787@gmail.com>
2024-03-14 18:29:17 -0400

    C - Variadic functions

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - a function that returns the sum of its parameters
+ * @n: const unsigned int
+ *
+ * Return: 0 if n is equal 0
+ */
+
+int sum_them_all(const unsigned int n,...)
+{
+	unsigned int i;
+	int total = 0;
+
+
+	va_list args;
+	va_start(args, n);
+
+	if (n == 0)
+		return(0);
+
+	for (i = 0; i < n; i++)
+	{
+		total += va_arg(args, int);
+	}
+	va_end(args);
+	return (total);
+}
