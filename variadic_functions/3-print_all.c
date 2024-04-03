52e60db2fb002252e619f9639af64b4a97c808d9
Your Name <saul.vera787@gmail.com>
2024-04-03 11:29:59 -0400

    correcting output

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -25,21 +25,24 @@ void print_all(const char * const format, ...)
 		{
 			case 'c':
 				printf("%s%c", separator, va_arg(args, int));
+				separator = ", ";
 				break;
 			case 'i':
 				printf("%s%d", separator, va_arg(args, int));
+				separator = ", ";
 				break;
 			case 'f':
 				printf("%s%f", separator, va_arg(args, double));
+				separator = ", ";
 				break;
 			case 's':
 				str = va_arg(args, char *);
 				if (!str)
 					str = "(nil)";
 				printf("%s%s", separator, str);
+				separator = ", ";
 				break;
 		}
-		separator = ", ";
 		i++;
 	}
 	va_end(args);
